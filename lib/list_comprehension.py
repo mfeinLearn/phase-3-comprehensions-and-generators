45ff96aa81ba3f9c424918efe300fa345916085e
Malcome Fein <mfein90@gmail.com>
2022-11-29 17:02:39 -0500

    Done.

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    list_comprehension = [n for n in num_list if n%2==0 ]
+    print(list_comprehension)
+    return list_comprehension
+
 
 def make_exclamation(sentence_list):
-    pass
+    list_comprehension = [n+'!' for n in sentence_list ]
+    print(list_comprehension)
+    return list_comprehension
